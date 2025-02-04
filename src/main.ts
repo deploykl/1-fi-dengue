0a7771948d05a12e63e2d1cd354283921cf40127
LUIS ANGEL SALAS LAZA <system.deploykl@gmail.com>
2025-02-04 16:52:19 -0500

    sdd

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,22 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';  
+
+//testedczxczcxzczcxzc
+import '@/assets/css/main.css'
+import '@/assets/css/aside.css'
+import '@/assets/js/aside.js'
+
+import '@/assets/lib/fontawesome-v6.5.2/css/all.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-light.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-regular.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-solid.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-thin.css'
+
+
 createApp(App).use(store).use(router).mount('#app')
