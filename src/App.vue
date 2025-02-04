c6cfbc3870ec6dc0f45958d1c6ebab733e1be8bf
LUIS ANGEL SALAS LAZA <system.deploykl@gmail.com>
2025-02-04 16:23:05 -0500

    yrd

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,126 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- HEADER -->
+  <header-component-vue v-if="showHeader" @toggleAside="toggleAside" />
+  <!-- Transición para el aside -->
+  <transition name="slide">
+    <aside-component-vue v-if="showAside" />
+  </transition>
+  <!-- NAVIGATION -->
+  <page-title-component-vue v-if="showPageTitle" />
+  <!-- MAIN CONTENT -->
+  <router-view />
+  <!-- <button-float-component-vue v-if="showButton" />-->
+  <!-- FOOTER -->
+  <footer-component-vue v-if="showFooter" />
 </template>
 
+<script setup>
+import { onMounted, computed, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import HeaderComponentVue from '@/layouts/HeaderComponent.vue';
+import FooterComponentVue from '@/layouts/FooterComponent.vue';
+import AsideComponentVue from '@/layouts/AsideComponent.vue';
+import PageTitleComponentVue from '@/layouts/PageTitleComponent.vue';
+//import ButtonFloatComponentVue from '@/layouts/widgets/ButtonFloatComponent.vue';
+
+// Importa los componentes
+const route = useRoute();
+
+// Estado para controlar la visibilidad del aside
+const isAsideVisible = ref(true);
+
+// Función para alternar la visibilidad del aside
+const toggleAside = () => {
+  if (route.name !== 'not-found' && route.path !== '/login' && route.path !== '/') {
+    isAsideVisible.value = !isAsideVisible.value;
+
+    // Alterna la clase en el body cuando el aside se muestra o se oculta
+    if (isAsideVisible.value) {
+      document.body.classList.add('sidebar-expanded');
+      document.body.classList.remove('sidebar-collapsed');
+    } else {
+      document.body.classList.remove('sidebar-expanded');
+      document.body.classList.add('sidebar-collapsed');
+    }
+  }
+};
+
+// Asegúrate de aplicar la clase adecuada al cargar la página
+onMounted(() => {
+  if (isAsideVisible.value) {
+    document.body.classList.add('sidebar-expanded');
+    document.body.classList.remove('sidebar-collapsed');
+  } else {
+    document.body.classList.remove('sidebar-expanded');
+    document.body.classList.add('sidebar-collapsed');
+  }
+});
+
+
+// Determina si se deben mostrar los componentes
+const showHeader = computed(() =>
+  route.path !== '/' &&
+  route.name !== 'not-found' &&
+  route.name !== 'login-poi' &&
+  route.name !== 'login-administracion' &&
+  route.name !== 'zoom' &&
+  route.path !== '/sgd/pendientes');
+
+const showAside = computed(() => isAsideVisible.value &&
+  route.path !== '/' &&
+  route.name !== 'not-found' &&
+  route.name !== 'login-poi' &&
+  route.name !== 'login-administracion' &&
+  route.name !== 'zoom' &&
+  route.path !== '/sgd/pendientes');
+
+const showPageTitle = computed(() =>
+  route.path !== '/' &&
+  route.name !== 'not-found' &&
+  route.name !== 'login-poi' &&
+  route.name !== 'login-administracion' &&
+  route.name !== 'zoom' &&
+  route.path !== '/sgd/pendientes');
+
+const showFooter = computed(() => 
+route.path !== '/' && 
+route.name !== 'not-found' && 
+route.name !== 'login-poi' && 
+route.name !== 'login-administracion' &&
+route.name !== 'zoom' &&
+route.path !== '/sgd/pendientes');
+//const showButton = computed(() => route.name !== 'not-found' && route.path !== '/login' && route.path !== '/');
+
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+  margin: 0;
 }
 
-nav {
-  padding: 30px;
+/* Estilos para la transición */
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease, opacity 0.3s ease;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.slide-enter,
+.slide-leave-to
+
+/* .slide-leave-active en <2.1.8 */
+  {
+  opacity: 0;
+  transform: translateX(-100%);
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.slide-enter-to,
+.slide-leave
+
+/* .slide-enter-active en <2.1.8 */
+  {
+  opacity: 1;
+  transform: translateX(0);
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -22,4 +22,21 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem('auth_token');  // Verificar si está autenticado
+  const isAdmin = localStorage.getItem('is_superuser') === 'true' || localStorage.getItem('is_staff') === 'true';  // Verificar si es admin
+
+  // Si la ruta requiere autenticación y no está autenticado
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'home' });  // Redirigir al home si no está autenticado
+  }
+  // Verificar si la ruta requiere permisos de administrador
+  else if (to.meta.isAdmin && !isAdmin) {
+    next({ name: 'home' });  // Redirigir al home si no es administrador
+  } 
+  else {
+    next();  // Continuar a la ruta solicitada si todo está bien
+  }
+});
+
 export default router
